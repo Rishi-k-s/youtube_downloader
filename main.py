a5cc9bfc39df5e3b4f50aec0e76468f7a0b897a8
Rishi-k-s <rishikrishna.sr@gmail.com>
2023-07-24 23:47:07 +0530

    so the error is when we put the base,ext=os.path... in the function that ext is not recogonised, have to fix

--- main.py
+++ main.py
@@ -72,6 +72,18 @@ def download_audio(audio_url_list):
 
         print("audio was downloaded successfully")
 
+def test_dwnld_audio (audio_link_list):
+    for each_audio_link in audio_link_list:
+        yt = YouTube("https://www.youtube.com/watch?v=eiGdsH1g20k")
+    video = yt.streams.filter(only_audio=True).first()
+    destination = 'audio'
+    out_file = video.download(destination)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print(yt.title + " has been successfully downloaded.")
+
+
 while True:
     print("[1] Download video")
     print("[2] Download Audio")
